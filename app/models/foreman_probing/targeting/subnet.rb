58c60cb4247472ac749b88096d8a8fd09cb3a9e4
Adam Ruzicka <a.ruzicka@outlook.com>
2017-09-22 14:21:16 +0200

    Fix displaying subnet type scans

--- app/models/foreman_probing/targeting/subnet.rb
+++ app/models/foreman_probing/targeting/subnet.rb
@@ -1,18 +1,17 @@
 module ForemanProbing
   class Targeting::Subnet < ForemanProbing::Targeting
-    # def initialize(subnet_id)
-    #   @subnet = ::Subnet.authorized.find(subnet_id)
-    # end
-
     def target_kind
       'subnet'
     end
 
+    def subnet
+      @subnet ||= ::Subnet.authorized.find(raw_targets.to_i)
+    end
+
     def targets
       # TODO: This is ugly
-      @subnet ||= ::Subnet.authorized.find(raw_targets.to_i)
-      cidr = IPAddr.new(@subnet.mask).to_i.to_s(2).count('1')
-      @targets ||= "#{@subnet.network}/#{cidr}"
+      cidr = IPAddr.new(subnet.mask).to_i.to_s(2).count('1')
+      @targets ||= "#{subnet.network}/#{cidr}"
     end
   end
 end
--- foreman_probing_core.gemspec
+++ foreman_probing_core.gemspec
@@ -19,6 +19,6 @@ Gem::Specification.new do |s|
   # s.add_dependency 'deface'
   # s.add_development_dependency 'rubocop'
   # s.add_development_dependency 'rdoc'
-  s.add_dependency('foreman-tasks-core', '~> 0.1.0')
+  s.add_dependency('foreman-tasks-core', '~> 0.1')
   s.add_dependency('ruby-nmap', '~> 0.9')
 end
