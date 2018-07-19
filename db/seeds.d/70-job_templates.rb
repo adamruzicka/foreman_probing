bf1338c499b1fa42faa4af185764cb9240ddffe6
Adam Ruzicka <aruzicka@redhat.com>
2018-07-19 11:25:05 +0200

    Issue #8 - Do not seed JobTemplates when REX is not enabled

--- db/seeds.d/70-job_templates.rb
+++ db/seeds.d/70-job_templates.rb
@@ -1,7 +1,9 @@
-User.as_anonymous_admin do
-  JobTemplate.without_auditing do
-    Dir[File.join("#{ForemanProbing::Engine.root}/app/views/templates/**/*.erb")].each do |template|
-      JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => false)
+if defined?(::JobTemplate)
+  User.as_anonymous_admin do
+    JobTemplate.without_auditing do
+      Dir[File.join("#{ForemanProbing::Engine.root}/app/views/templates/**/*.erb")].each do |template|
+        JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => false)
+      end
     end
   end
 end
