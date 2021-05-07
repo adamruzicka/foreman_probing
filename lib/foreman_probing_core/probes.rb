374b78b994aeeefd975ff8d92d45c1bb324a08db
Adam Ruzicka <aruzicka@redhat.com>
2021-05-07 12:37:17 +0200

    Drop foreman_probing_core

--- lib/foreman_probing_core/probes.rb
+++
@@ -1,9 +0,0 @@
-module ForemanProbingCore
-  module Probes
-    require 'foreman_probing_core/probes/abstract'
-    require 'foreman_probing_core/probes/nmap'
-    require 'foreman_probing_core/probes/tcp'
-    require 'foreman_probing_core/probes/udp'
-    require 'foreman_probing_core/probes/icmp'
-  end
-end
